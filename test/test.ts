f08c2a5c6a3f83a8c542e1e220dfe699dc843098
Nikita Kuznetsov <n@8gen.team>
2022-12-18 20:59:05 +0100

    chore: simplify, remove hardcoded address

--- test/test.ts
+++ test/test.ts
@@ -3,12 +3,8 @@ import { FakeContract, smock } from '@defi-wonderland/smock';
 import { ethers, network, artifacts } from "hardhat";
 import { expect } from 'chai';
 import * as chai from "chai";
-// @ts-ignore
-import { constants } from  "@openzeppelin/test-helpers";
 import { A as Contract, B, A__factory as ContractFactory } from "../typechain";
 
-const { ZERO_ADDRESS } = constants;
-const B_ADDRESS = "0x0000000000FFe8B47B3e2130213B802212439497";
 
 chai.should(); // if you like should syntax
 chai.use(smock.matchers);
@@ -22,15 +18,12 @@ const createTestSuite = ({ name }: { name: string }) =>
     function () {
         before(async () => {
             factory = await ethers.getContractFactory(name) as ContractFactory;
-            contract = await factory.deploy() as Contract;
+            fakeB = await smock.fake("B");
+            contract = await factory.deploy(fakeB.address) as Contract;
             await contract.deployed();
         });
 
         context(`${name}`, function () {
-            beforeEach(async function() {
-                fakeB = await smock.fake("B", {address: B_ADDRESS});
-            });
-
             it("should be reverted with message", async function() {
                 fakeB.test.reverts(`Message`);
                 await expect(contract.test())
